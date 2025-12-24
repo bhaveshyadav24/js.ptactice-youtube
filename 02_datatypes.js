7e26d79034b9a0dca23e64b56e354ad9d2befad3
bhaveshyadav24 <144918265+bhaveshyadav24@users.noreply.github.com>
2025-12-24 18:33:33 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "bhavesh"
+let age = 24
+let isLoggedIn = true
+
+// premetive data type
+
+// number => 2 to power 53
+// bigint it 
+//string => "" use it in double code name a to z  
+// boolean => true /false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);
